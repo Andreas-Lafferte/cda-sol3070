4a400dc35040dbc0cc97aaf4c47fa67ef7f8bb25
Andreas Lafferte <andreas.lafferte.tamayo@gmail.com>
2024-08-28 17:00:46 -0400

    push tareita 2

--- class/convert_classes.R
+++ class/convert_classes.R
@@ -3,7 +3,16 @@
 library(pagedown)
 library(here)
 
-# Clase 1
+# Clases
 
 pagedown::chrome_print('https://mebucca.github.io/cda_soc3070/slides/class_1/class_1#1', 
                        output = here("class", "class_1.pdf"))
+
+pagedown::chrome_print('https://mebucca.github.io/cda_soc3070/slides/class_2/class_2#1', 
+                       output = here("class", "class_2.pdf"))
+
+pagedown::chrome_print('https://mebucca.github.io/cda_soc3070/slides/class_3/class_3#1', 
+                       output = here("class", "class_3.pdf"))
+
+pagedown::chrome_print('https://mebucca.github.io/cda_soc3070/slides/class_4/class_4#1', 
+                       output = here("class", "class_4.pdf"))
